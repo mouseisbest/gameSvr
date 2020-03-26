857f21e5c95a41fc5ef6705bf5e69656d5bbc529
Walter <mouseisbest@gmail.com>
2020-03-26 22:39:24 +0800

    temp protool bash file

---
+++ src/protocol/gen.sh
@@ -0,0 +1,6 @@
+protoc -I ./ --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` ./def.proto
+protoc -I ./ --cpp_out=. ./def.proto
+protoc -I ./ --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` ./res.proto
+protoc -I ./ --cpp_out=. ./def.proto
+protoc -I ./ --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` ./cs.proto
+protoc -I ./ --cpp_out=. ./cs.proto
